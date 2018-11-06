bbbe2b43a99a2e9c42cc050e0f99da17f65a0da2
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2018-11-06 17:10:45 +0100

    Commencement de l'affichage des prestations selon une catégorieé

--- js/catalog.js
+++ js/catalog.js
@@ -1,2 +1,20 @@
 let btnAttention = document.getElementById("btnAttention");
 let btnActivite = document.getElementById("btnActivite");
+let btnRestauration = document.getElementById("btnRestauration");
+let btnHebergement = document.getElementById("btnHebergement");
+
+btnAttention.onclick = function() {
+    alert('attention');
+};
+
+btnActivite.onclick = function() {
+    alert('activite');
+};
+
+btnRestauration.onclick = function() {
+    alert('restauration');
+};
+
+btnHebergement.onclick = function() {
+    alert('hebergement');
+};
