470fb9d6119fbffdfcacb47c7fcf960e539110b1
Maeva <butaye.maeva@gmail.com>
2018-11-07 14:56:08 +0100

    js tri catalogue

--- js/catalog.js
+++ js/catalog.js
@@ -2,59 +2,66 @@ let btnAttention = document.getElementById("btnAttention");
 let btnActivite = document.getElementById("btnActivite");
 let btnRestauration = document.getElementById("btnRestauration");
 let btnHebergement = document.getElementById("btnHebergement");
+let selectTri = document.getElementById("tri");
 
 let tabPrestations = [];
 
 btnAttention.onclick = function() {
     if ($(':first', this).hasClass("actif")) {
         $(':first', this).removeClass("actif");
-        renderPrestaCateg(tabPrestations);
+        tabPrestVariable =tabPrestations;
     } else {
         $(".actif").removeClass("actif");
-        renderPrestaCateg(equalCategory('Attention'));
+        equalCategory('Attention')
         $(':first', this).addClass("actif");
     }
+    tri(selectTri.value);
 };
 
 btnActivite.onclick = function() {
     if ($(':first', this).hasClass("actif")) {
         $(':first', this).removeClass("actif");
-        renderPrestaCateg(tabPrestations);
+        tabPrestVariable =tabPrestations;
     } else {
         $(".actif").removeClass("actif");
-        renderPrestaCateg(equalCategory('Activité'));
+        equalCategory('Activité')
         $(':first', this).addClass("actif");
     }
+    tri(selectTri.value);
 };
 
 btnRestauration.onclick = function() {
     if ($(':first', this).hasClass("actif")) {
         $(':first', this).removeClass("actif");
-        renderPrestaCateg(tabPrestations);
+        tabPrestVariable =tabPrestations;
     } else {
         $(".actif").removeClass("actif");
-        renderPrestaCateg(equalCategory('Restauration'));
+        equalCategory('Restauration')
         $(':first', this).addClass("actif");
     }
+    tri(selectTri.value);
 };
 
 btnHebergement.onclick = function() {
     if ($(':first', this).hasClass("actif")) {
         $(':first', this).removeClass("actif");
-        renderPrestaCateg(tabPrestations);
+        tabPrestVariable =tabPrestations;
     } else {
         $(".actif").removeClass("actif");
-        renderPrestaCateg(equalCategory('Hébergement'));
+        equalCategory('Hébergement')
         $(':first', this).addClass("actif");
     }
+    tri(selectTri.value);
 };
 
 $('.tabPrestations').each(function() {
     tabPrestations.push($(this));
 });
 
+let tabPrestVariable = tabPrestations;
+
 function equalCategory(element) {
-    return tabPrestations.filter(function(prest) {
+    tabPrestVariable = tabPrestations.filter(function(prest) {
         return prest.children()[2]['innerHTML'] === element;
     });
 }
@@ -64,4 +71,28 @@ function renderPrestaCateg(tableau) {
     for (let i = 0; i < tableau.length; i++) {
         $('.grid_container_prestations').append(tableau[i]);
     }
+}
+
+function tri(value){
+    let tab = new Array();
+    tabPrestVariable.forEach(function (t){
+        tab.push(t);
+    });
+    if (value === "0"){
+        renderPrestaCateg(tabPrestVariable);
+    }
+    else if (value === "1"){
+        renderPrestaCateg(tab.sort(increasing));
+    }
+    else{
+        renderPrestaCateg(tab.sort(decrease));
+    }
+}
+
+function increasing(x, y) {
+    return x.children()[3]['innerHTML'] - y.children()[3]['innerHTML'];
+}
+
+function decrease(x, y) {
+    return y.children()[3]['innerHTML'] - x.children()[3]['innerHTML'];
 }
