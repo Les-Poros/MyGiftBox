676a9cd2bd91eca457042bd075fc8acd988c9d9b
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2018-11-09 15:46:04 +0100

    Modif model

--- src/models/ContenuCoffret.php
+++ src/models/ContenuCoffret.php
@@ -8,7 +8,7 @@ namespace MyGiftBox\models;
 class ContenuCoffret extends \Illuminate\Database\Eloquent\Model {
 
     protected $table = 'contenuCoffret';
-    protected $primaryKey = ['idCoffret', 'idPrestation'];
+    protected $primaryKey = 'idContenuCoffret';
     public $timestamps = false;
 
     public function coffret() {
